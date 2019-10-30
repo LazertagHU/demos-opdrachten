010cf5c2fe786e8bd845ccc690657dfffda99b49
Nathan Houwaart <nathan.houwaart@student.hu.nl>
2019-10-30 17:14:33 +0100

    added comments

--- runGameTaak/runGameTaak.hpp
+++ runGameTaak/runGameTaak.hpp
@@ -69,27 +69,79 @@ private:
     rtos::clock                 secondClock;
     rtos::timer                 delayTimer;
 
+    /*
+    * Main function of this task
+    * This task controls the game setup and the game whilst it's running
+    * 
+    * It's code is based on the runGameTaak std and can be found in the
+    * google drive map.
+    */
     void main() override;
 
-
+    /*
+    * The game leader can send a specific command which states the length
+    * of the upcoming game.
+    * This function decodes that command and returns the gametime. 
+    */
     int computeGameTime(int msg);
 
+    /*
+    * The game leader can send a specific command which tells the player 
+    * in how many seconds the game is going to start. This way, every
+    * player starts at about the same time.
+    * 
+    * This function decodes that message and translates it in to the proper
+    * countdown.
+    */
     int computeCountdown(int msg);
 
+    /*
+    * The game leader can send a specific command which tells the player 
+    * in how many seconds the game is going to start. This way, every
+    * player starts at about the same time.
+    * 
+    * This function calculates the specific command that needs to be send 
+    * by the game leader at a specific countdown timer.
+    * 
+    */
     int computeStartCommand(int countdown);
 
+    /*
+    * This function adds two inputs of the keypad together.
+    * The user has the option to return when no numbers are entered
+    * or when one number is entered. this way the user can return
+    * 1 - 9 and then return out of the function.
+    */
     int waitForInput();
    
+    /*
+    * This function calculates the checksum of a specific command.
+    * This is done by XOR-ing specific bits
+    */
     uint32_t calculateCheckSum(uint32_t input);
 
+    /*
+    * This function is used to decide wether te player is hit
+    */
     bool isHitMessage(uint32_t message);
     
+    /*
+    * This function is used to calculate the hit damage when a
+    * player is hit
+    */
     int computeHit(uint32_t message);
 
+    /*
+    * This function is used to calculate the delay that a player
+    * can't shoot or be shot after he has been shot by another player
+    */
     int computeDelay(uint32_t message);
     
 
 public:
+    /*
+    * Constructor of RunGameTaak
+    */
     RunGameTaak(
         Display & display, 
         Transmitter& transmitter
